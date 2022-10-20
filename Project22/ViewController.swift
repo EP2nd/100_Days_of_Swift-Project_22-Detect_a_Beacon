bdc2f5f17e4d5c09820f834911e03a86ec54ce6c
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-10-20 22:34:39 +0200

    Initial Commit

---
+++ Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Edwin Przeźwiecki Jr. on 20/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
