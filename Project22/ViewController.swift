38f463d660f7a363ed28c360a4c44f0db93ab1c0
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-10-22 23:56:46 +0200

    Almost finished second challenge

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -10,7 +10,10 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var distanceReading: UILabel!
-    
+    // Challenge 2:
+    @IBOutlet var beaconName: UILabel!
+    // Challenge 2:
+    var currentBeaconUUID: UUID?
     var locationManager: CLLocationManager?
     
     // Challenge 1:
@@ -24,6 +27,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager?.requestAlwaysAuthorization()
         
         view.backgroundColor = .gray
+        distanceReading.text = "UNKNOWN"
+        beaconName.text = "No beacon detected"
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -35,20 +40,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
-    
-    func startScanning() {
-        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        //let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
-        let identityConstraint = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
-        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: identityConstraint, identifier: "MyBeacon")
+    // Challenge 2:
+    func addBeacon(uuidString: String, major: CLBeaconMajorValue, minor: CLBeaconMinorValue, identifier: String) {
+        let uuid = UUID(uuidString: uuidString)!
+        let identityConstraint = CLBeaconIdentityConstraint(uuid: uuid, major: major, minor: minor)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: identityConstraint, identifier: identifier)
         
         locationManager?.startMonitoring(for: beaconRegion)
-        //locationManager?.startRangingBeacons(in: beaconRegion)
         locationManager?.startRangingBeacons(satisfying: identityConstraint)
     }
+    // Challenge 2:
+    func startScanning() {
+        addBeacon(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: 123, minor: 456, identifier: "Ed")
+        addBeacon(uuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0", major: 789, minor: 123, identifier: "Edd")
+        addBeacon(uuidString: "74278BDA-B644-4520-8F0C-720EAF059935", major: 456, minor: 789, identifier: "Eddy")
+    }
     
-    func update(distance: CLProximity) {
+    func update(distance: CLProximity, identifier: String) {
         UIView.animate(withDuration: 0.8) {
+            self.beaconName.text = "\(identifier)"
+            
             switch distance {
             case .far:
                 self.view.backgroundColor = .blue
@@ -65,12 +76,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             default:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
+                self.beaconName.text = "No beacon detected."
             }
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
+            if currentBeaconUUID == nil { currentBeaconUUID = region.uuid }
+            //guard currentBeaconUUID == region.uuid else { return }
             // Challenge 1:
             if firstBeaconDetectedtAlertShown == false {
                 let ac = UIAlertController(title: "Beacon detected", message: "Follow the clues if you wish to get near it.", preferredStyle: .alert)
@@ -78,9 +92,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 present(ac, animated: true)
                 firstBeaconDetectedtAlertShown = true
             }
-            update(distance: beacon.proximity)
+            update(distance: beacon.proximity, identifier: region.identifier)
         } else {
-            update(distance: .unknown)
+            guard currentBeaconUUID == region.uuid else { return }
+            currentBeaconUUID = nil
+            update(distance: .unknown, identifier: "No beacon detected.")
         }
     }
 }
